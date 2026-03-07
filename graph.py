2270423eabe7a3138fe3604974126f775b9721a5
Valt2020shu <valt2020shu@gmail.com>
2026-03-08 00:02:16 +0530

    Graph Finals

--- graph.py
+++ graph.py
@@ -20,11 +20,11 @@ time = datetime.now()
 for days in file_info[f"{time.year}-{time.strftime("%U")}"].values():
     for hours in days:  
         frequency[days[hours]["Category"]] += 1
-chart_data = {"Categories": [], "HoursSpent": [], "IdealTime":[]}
+chart_data = {"Categories": [], "Hours Spent": [], "Ideal Time":[]}
 for i in frequency:
     chart_data["Categories"].append(i)
-    chart_data["HoursSpent"].append(frequency[i])
-    chart_data["IdealTime"].append(ideal[i])
+    chart_data["Hours Spent"].append(frequency[i])
+    chart_data["Ideal Time"].append(ideal[i])
     
 
 bar = sns.barplot(data=chart_data, x= "Categories", y = "Hours Spent", hue="Categories")
@@ -45,6 +45,12 @@ chart_df = pd.DataFrame(chart_data)
 
 line_1 = sns.lineplot(data=chart_df, x="Categories", y = "Hours Spent", label= "Hours Spent", markers=True, marker="o")
 line_2 = sns.lineplot(data=chart_df, x="Categories",y = "Ideal Time", label = 'Ideal Time', markers=True, marker="o")
+for i, row in chart_df.iterrows():
+    plt.text(i, row['Hours Spent'] + 0.5, f"{row['Hours Spent']}", 
+             ha='center', va='bottom', fontweight='bold', color='blue')
+    
+    plt.text(i, row['Ideal Time'] - 0.5, f"{row['Ideal Time']}", 
+             ha='right', va='top', fontweight='bold', color='orange')
 plt.title(f"{time.year}-{time.strftime("%U")}")
 plt.legend()
 
